2e716dbb3bf8fff1200c0f231c28c027b443863d
Louis <louis.drotos@gmail.com>
2024-07-05 14:03:59 -0400

    Added apparatus service as a sub-project

---
+++ services/apparatus-service/src/main/java/org/ffx/fire/apparatus_service/ApparatusServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ffx.fire.apparatus_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparatusServiceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApparatusServiceApplication.class, args);
+	}
+	
+}
---
+++ services/apparatus-service/src/test/java/org/ffx/fire/apparatus_service/ApparatusServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package org.ffx.fire.apparatus_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApparatusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
